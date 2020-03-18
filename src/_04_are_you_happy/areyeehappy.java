6005f79feece755811532316fe04f6c9f4e9741e
root <root@e61ef80c18ab>
2020-03-18 21:59:53 +0000

    star sign

--- src/_04_are_you_happy/areyeehappy.java
+++ src/_04_are_you_happy/areyeehappy.java
@@ -3,58 +3,47 @@ package _04_are_you_happy;
 import javax.swing.JOptionPane;
 
 public class areyeehappy {
-public static void main(String[] args) {
-	
-	String input = JOptionPane.showInputDialog("Are you happy??");
-	
-	
-	if(input .equals ("yes")) {
-		
-		
-		JOptionPane.showMessageDialog(null, "Keep doing whatever your doing");
-	}
-	
-	
+	public static void main(String[] args) {
 
-	
+		String input = JOptionPane.showInputDialog("Are you happy??");
 
-	else if(input .equals ("no")) {
+		if (input.equals("yes")) {
+
+			JOptionPane.showMessageDialog(null, "Keep doing whatever your doing");
+		}
+
+		else if (input.equals("no")) {
+
+			input = JOptionPane.showInputDialog("Do you want to be happy?");
+
+			if (input.equals("no")) {
+
+				JOptionPane.showMessageDialog(null, "Keep doing whatever your doing");
+			
+			
+			
+			
+			
+			
+			}
 		
 		
-		JOptionPane.showInputDialog("Do you want to be happy?");
+		
+			else if  (input.equals("yes")) {
+
+				JOptionPane.showMessageDialog(null, "Change something");
+					
 				
-	}
-	
-	
-	
-	
+		
+			}
+		
+		}
+		
+		
+		
+		
+		
+		}
 
-	else if(input .equals ("yes")) {
-		
-		
-		JOptionPane.showMessageDialog(null, "Change something"); 
-	}
 	
-	
-	
-	
-	
-	
-else if(input .equals ("no")) {
-		
-		
-		JOptionPane.showMessageDialog(null, "Keep doing whatever your doing");
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-}
 }
---
+++ src/_05_horoscope/horoscope.java
@@ -0,0 +1,54 @@
+package _05_horoscope;
+
+import javax.swing.JOptionPane;
+
+public class horoscope {
+public static void main(String[] args) {
+	
+	
+	
+	String input = JOptionPane.showInputDialog("What is your star sign?");
+	
+	
+	
+	
+	if(input .equals ("Aries")) {
+		
+		JOptionPane.showMessageDialog(null, "You will be very rich one day");
+	}
+	
+	
+	
+	
+	else if(input .equals ("Taurus")){
+		
+		JOptionPane.showMessageDialog(null, "You will never become rich");
+	}
+	
+	
+	
+	else if(input .equals ("Gemini")){
+		
+		JOptionPane.showMessageDialog(null, "You will trip one day");
+	}
+	
+	
+	else if(input .equals ("Cancer")) {
+		JOptionPane.showMessageDialog(null, "You will never trip");
+	}
+	
+	
+	else if(input .equals))
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
