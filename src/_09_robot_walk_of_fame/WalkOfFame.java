df78998da69f0ad83a4cfaf728bf4ee7547a96a4
root <root@b69aeb9a4b92>
2020-04-08 21:59:23 +0000

    e

--- src/_09_robot_walk_of_fame/WalkOfFame.java
+++ src/_09_robot_walk_of_fame/WalkOfFame.java
@@ -11,10 +11,128 @@ public class WalkOfFame {
 		Robot rob = new Robot();
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
-
+rob.setX(100);
 		// 2. Make the robot draw a star shape. Hint: 144.
+rob.penDown();
+rob.setSpeed(200);
+for (int i = 0; i < 5; i++) {
 
-		// 3. Set the size of the star to 30.
+rob.move(30);
+rob.turn(144);
+}
+
+
+
+rob.setX(150);
+rob.penDown();
+rob.setSpeed(200);
+for (int i = 0; i < 5; i++) {
+
+rob.move(30);
+rob.turn(144);
+}
+
+
+
+
+rob.setX(200);
+rob.penDown();
+rob.setSpeed(200);
+for (int i = 0; i < 5; i++) {
+
+rob.move(30);
+rob.turn(144);
+}
+
+
+
+
+rob.setX(250);
+rob.penDown();
+rob.setSpeed(200);
+for (int i = 0; i < 5; i++) {
+
+rob.move(30);
+rob.turn(144);
+}
+
+
+rob.setX(300);
+rob.penDown();
+rob.setSpeed(200);
+for (int i = 0; i < 5; i++) {
+
+rob.move(30);
+rob.turn(144);
+}
+
+
+
+rob.setX(350);
+rob.penDown();
+rob.setSpeed(200);
+for (int i = 0; i < 5; i++) {
+
+rob.move(30);
+rob.turn(144);
+}
+
+
+
+rob.setX(400);
+rob.penDown();
+rob.setSpeed(200);
+for (int i = 0; i < 5; i++) {
+
+rob.move(30);
+rob.turn(144);
+}
+
+
+
+rob.setX(450);
+rob.penDown();
+rob.setSpeed(200);
+for (int i = 0; i < 5; i++) {
+
+rob.move(30);
+rob.turn(144);
+}
+
+
+
+
+rob.setX(500);
+rob.penDown();
+rob.setSpeed(200);
+for (int i = 0; i < 5; i++) {
+
+rob.move(30);
+rob.turn(144);
+}
+
+
+
+rob.setX(550);
+rob.penDown();
+rob.setSpeed(200);
+for (int i = 0; i < 5; i++) {
+
+rob.move(30);
+rob.turn(144);
+}
+
+
+
+
+
+
+
+
+
+
+
+// 3. Set the size of the star to 30.
 
 		/** THE CHALLENGE: **/
 		/*
